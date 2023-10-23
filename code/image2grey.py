418ec7de1cb3c941511014b5aa046596a6f7f301
clean <2238650780@qq.com>
2023-10-23 13:47:55 +0800

    图片处理测试通过完成

--- code/image2grey.py
+++ code/image2grey.py
@@ -1,10 +1,10 @@
 import cv2
-import numpy as np
+import numpy
+# 导入PIL库 加载字体和绘制文本
+from PIL import Image, ImageDraw, ImageFont
 
 
 def img2gray(color_image):
-    # 将图像转为灰度
-
     # 将彩色图片转换为灰度图片
     gray_img = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 
@@ -15,46 +15,54 @@ def img2gray(color_image):
 
 def img_invert(img):
     # 将黑色和白色交换
-
     inverted_img = cv2.bitwise_not(img)
     return inverted_img
 
 
-def add_text(img, text):
-    # 选择字体和字号
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 1
-    font_thickness = 2
+def cv2ImgAddText(img, text, textColor=(255, 0, 0), fontSize=36):
+    if isinstance(img, numpy.ndarray):  # 判断是否OpenCV图片类型
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # 创建一个可以在给定图像上绘图的对象
+    draw = ImageDraw.Draw(img)
 
-    # 获取文本的大小
-    text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+    # 将文本添加到紧贴图片右侧的位置 因此计算图片占用像素宽度
+    pixel = 0
+    for i in text:
+        if i.isascii():
+            pixel += 0.5
+        else:
+            pixel += 1
+    pixel *= fontSize
 
-    # 计算文本位置（右上角）
-    text_x = img.shape[1] - text_size[0] - 30  # 30像素的边距
-    text_y = 30  # 30像素的边距
+    # 设置字体
+    fontPath = "../source/font/No.39-ShangShouZhiZunShuFaTi-2.ttf"
+    # 字体的格式
+    fontStyle = ImageFont.truetype(fontPath, fontSize)
+    # 文本位置
+    text_position = (image.shape[1] - pixel, 50)  # 10像素的边距
+    # 绘制文本
+    draw.text(text_position, text, fill=textColor, font=fontStyle)
+    # 转换回OpenCV格式
+    return cv2.cvtColor(numpy.asarray(img), cv2.COLOR_RGB2BGR)
 
-    # 指定文本颜色
-    text_color = (0, 0, 255)
 
-    # 在图像上绘制文本
-    cv2.putText(img, text, (text_x, text_y), font, font_scale, text_color, font_thickness)
-    return img
+if __name__ == '__main__':
+    imageName = "frame_1.jpg"  # 待处理的图片
+    # 读取图像
+    image = cv2.imread('../source/input_Image/'+imageName)
 
+    # 将图片灰度处理
+    gray_image = img2gray(image)
+    # 将图片的黑白反转
+    inverted_image = img_invert(gray_image)
 
-imageName = "frame_1.jpg"
-imgText = "21200107209冯鑫钢"
-# 读取图像
-image = cv2.imread('../source/input_Image/'+imageName)
-# 将图片灰度处理
-gray_image = img2gray(image)
+    # 为图片添加文本
+    imgText = "21200107209冯鑫钢"  # 要写入图片的文本
+    image_okay = cv2ImgAddText(inverted_image,  imgText)
+    # 保存结果
+    cv2.imwrite('../source/output_Image/'+imageName, image_okay)
 
-inverted_image = img_invert(gray_image)
-
-image_okay = add_text(inverted_image, imgText)
-# 保存结果
-# cv2.imwrite('../source/output_Image/'+imageName, image_okay)
-
-# 显示结果（可选）
-cv2.imshow('image', image_okay)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # 显示结果（可选）
+    cv2.imshow('My Report Picture', image_okay)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
